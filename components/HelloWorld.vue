7e5abf07d4950d18e60366786b1768903a1eab62
Adam Silva <as@skeeled.com>
2020-08-28 11:03:16 +0100

    Initial Commit

---
+++ components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <h1 class="hello">Hello Nuxt Community</h1>
+</template>
+
+<script>
+export default {
+    name: 'hello-world'
+};
+</script>
+
+<style>
+  :root{
+    --hello-world-text: red;
+  }
+  .hello {
+    color: var(--hello-world-text);
+    background-color: var(--hello-world-bg);
+  }
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1 class="title">
+        nuxt-ie11-css-variables
+      </h1>
+      <hello-world></hello-world>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+  :root{
+    --hello-world-bg: green;
+  }
+
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+
+.title {
+  font-family:
+    'Quicksand',
+    'Source Sans Pro',
+    -apple-system,
+    BlinkMacSystemFont,
+    'Segoe UI',
+    Roboto,
+    'Helvetica Neue',
+    Arial,
+    sans-serif;
+  display: block;
+  font-weight: 300;
+  font-size: 100px;
+  color: #35495e;
+  letter-spacing: 1px;
+}
+</style>
